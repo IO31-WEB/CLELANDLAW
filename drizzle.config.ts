fffd429e35fc1002ea701bc34ac92ef8911d110b
IO31-WEB <ISAIAHOLIVER95@YAHOO.COM>
2026-05-14 11:53:13 -0400

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,9 @@
 import type { Config } from "drizzle-kit";
 
 export default {
-  schema:    "./src/lib/schema.ts",
-  out:       "./drizzle",
-  driver:    "pg",
+  schema: "./src/lib/schema.ts",
+  out: "./drizzle",
+  dialect: "postgresql",     
   dbCredentials: {
     connectionString: process.env.DATABASE_URL!,
   },
